5e09fe85bc78bdc25e7af49d43d9026e1912382a
Nevisinn <igor.shub93@mail.ru>
2026-03-31 14:06:44 +0500

    Добавлен enum с поддерживаемыми типами

---
+++ kontur-hack2026/Models/SupportedTypes.cs
@@ -0,0 +1,15 @@
+﻿namespace kontur_hack2026.Models;
+
+public enum SupportedTypes
+{
+    Int,
+    Float,
+    Double,
+    String,
+    Boolean,
+    DateTime,
+    List,
+    Array,
+    Dictionary,
+    Object
+}
